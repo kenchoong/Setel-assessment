73753b5b685d919974f45039c5e687f531d1730a
Ken choong <matchartproduction@gmail.com>
2021-11-18 21:34:38 +0800

    Update cdk.ts

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -6,8 +6,8 @@ import { CdkStack } from "../lib/cdk-stack";
 const app = new cdk.App();
 
 const demoStack = {
-  account: "147816830747", // here need to put in ur account
-  region: "ap-southeast-1", // and ur region
+  account: "", // here need to put in ur account
+  region: "", // and ur region
 };
 
 new CdkStack(app, "CdkStack", { env: demoStack });
